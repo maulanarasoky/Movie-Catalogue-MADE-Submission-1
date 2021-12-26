f384e167525624542ed358974469d82667743eef
maulanarasoky <rasoky.maulana@gmail.com>
2021-12-26 18:29:26 +0700

    Show movies in Home Fragment

--- app/src/main/java/com/example/moviecataloguemadesubmission1/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviecataloguemadesubmission1/home/HomeFragment.kt
@@ -5,19 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.moviecataloguemadesubmission1.R
+import com.example.moviecataloguemadesubmission1.core.data.Resource
+import com.example.moviecataloguemadesubmission1.core.ui.MovieAdapter
+import com.example.moviecataloguemadesubmission1.databinding.FragmentHomeBinding
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
+
+    private val homeViewModel: HomeViewModel by viewModel()
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+
+            val movieAdapter = MovieAdapter()
+            movieAdapter.onItemClick = {
+
+            }
+
+            homeViewModel.movies.observe(viewLifecycleOwner, { movies ->
+                if (movies != null) {
+                    when (movies) {
+                        is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
+                        is Resource.Success -> {
+                            binding.progressBar.visibility = View.GONE
+                            movieAdapter.setData(movies.data)
+                        }
+                        is Resource.Error -> {
+                            binding.progressBar.visibility = View.GONE
+                            binding.tvError.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            })
+
+            with(binding.rvMovie) {
+                layoutManager = GridLayoutManager(context, 2)
+                setHasFixedSize(true)
+                adapter = movieAdapter
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
